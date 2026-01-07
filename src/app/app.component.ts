4aceab7fd27809aa33042bbcf965c1eded418400
Michele Pigurim <michelepigurim26@gmail.com>
2026-01-07 17:57:33 -0300

    desenvolvendo a aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { BannerComponent } from './banner/banner.component';
+import { FormNovaTransacaoComponent } from './form-nova-transacao/form-nova-transacao.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { TipoTransacao, Transacao } from './modelos/transacao';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [BannerComponent, FormNovaTransacaoComponent, ExtratoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'anybank';
+  transacoes = signal<Transacao[]>([]);
+
+  saldo = computed(() => {
+    return this.transacoes().reduce((acc, trasacaoAtual) => {
+      switch (trasacaoAtual.tipo) {
+        case TipoTransacao.DEPOSITO:
+          return acc + trasacaoAtual.valor;
+
+        case TipoTransacao.SAQUE:
+          return acc - trasacaoAtual.valor;
+
+        default:
+          throw new Error('Tipo de transação não identificado.');
+      }
+    }, 0);
+  });
+
+  processarTransacao(transacao: Transacao) {
+    this.transacoes.update((listaAtual) => [transacao, ...listaAtual]);
+  }
 }
